95b71bd901b9d66507deec769acb8a10eec97d83
Binod Kumar <kumarbinod21011983@gmail.com>
2026-05-21 12:55:45 +0000

    gmr project

--- gmr/Component.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define(["sap/ui/core/UIComponent","app/model/models"], function(UIComponent, models){
-"use strict";
-return UIComponent.extend("app.Component",{
-metadata:{manifest:"json"},
-init:function(){UIComponent.prototype.init.apply(this,arguments);
-this.setModel(models.createJSONModel());}
-});});
