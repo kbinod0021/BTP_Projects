7a143cb27a4e9cce4c16c43561469c3f412dc9d8
Binod Kumar <kumarbinod21011983@gmail.com>
2026-06-02 08:24:15 +0000

    gmr change

--- gmr_project/webapp/controller/CommonMenu.controller.js
+++ gmr_project/webapp/controller/CommonMenu.controller.js
@@ -4,90 +4,276 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("gmrproject.controller.CommonMenu", {
-       onInit: function () {
-
-    this._attachMenuClick();
-
-// this._resetMenu();
-
-//   var oRouter = this.getOwnerComponent().getRouter();
-
-//     oRouter.getRoute("Routemain").attachPatternMatched(function () {
-//         oRouter.navTo("studio", {}, true); // ✅ redirect to studio
-//     });
-
-},
-
-_resetMenu: function () {
-    var oMenu = this.byId("studio");
-    if (oMenu) {
-        oMenu.setSelectedKey(""); 
-    }
-},
+        onInit: function () {
 
 
-_attachMenuClick: function () {
+            const oData = {
+                menuItems: [
+                    {
+                        id: "studioItem",
+                        route: "studio",
+                        icon: "C",
+                        title: "PPA Configuration Studio",
+                        subText: "setup",
+                        badge: "",
+                        active: true,
+                        isMainMenu:true
+                    },
+                    {
+                        id: "inputItem",
+                        route: "input",
+                        icon: "I",
+                        title: "Input for Calculation",
+                        subText: "DC • SE • Screen",
+                        badge: "1",
+                        active: false,
+                        isMainMenu:true
+                    },
+                    {
+                        id: "outputItem",
+                        route: "output",
+                        icon: "O",
+                        title: "Output Center",
+                        subText: "documents",
+                        badge: "0",
+                        active: false,
+                        isMainMenu:true
+                    },
+                    {
+                        id: "onboardItem",
+                        route: "onboard",
+                        icon: "N",
+                        title: "Onboard New PPA",
+                        subText: "documents",
+                        badge: "0",
+                        active: false,
+                        isMainMenu:true
+                    },
 
-    var aItems = ["studioItem", "inputItem", "outputItem","OnboardItem", "appsItem"];
+                    {
+                        id: "onboardItem",
+                        route: "onboard",
+                        icon: "N",
+                        title: "Onboard New PPA",
+                        subText: "documents",
+                        badge: "0",
+                        active: false,
+                        isMainMenu:true
+                    },
+                    {
+                        id: "onboardItem",
+                        route: "onboard",
+                        icon: "N",
+                        title: "Onboard New PPA",
+                        subText: "documents",
+                        badge: "0",
+                        active: false,
+                        isMainMenu:true
+                    },
+                    {
+                        id: "onboardItem",
+                        route: "onboard",
+                        icon: "N",
+                        title: "Onboard New PPA",
+                        subText: "documents",
+                        badge: "0",
+                        active: false,
+                        isMainMenu:true
+                    },
+                    {
+                        id: "onpdfItem",
+                        route: "pdf",
+                        icon: "N",
+                        title: "PDF Viewer",
+                        subText: "documents",
+                        badge: "0",
+                        active: false,
+                        isMainMenu:false
+                    },
+                    {
+                        id: "onboardItem",
+                        route: "onboard",
+                        icon: "N",
+                        title: "Onboard New PPA",
+                        subText: "documents",
+                        badge: "0",
+                        active: false,
+                        isMainMenu:false
+                    },
+                    {
+                        id: "onboardItem",
+                        route: "onboard",
+                        icon: "N",
+                        title: "Onboard New PPA",
+                        subText: "documents",
+                        badge: "0",
+                        active: false,
+                        isMainMenu:false
+                    },
+                    {
+                        id: "onboardItem",
+                        route: "onboard",
+                        icon: "N",
+                        title: "Onboard New PPA",
+                        subText: "documents",
+                        badge: "0",
+                        active: false,
+                        isMainMenu:false
+                    },
+                    {
+                        id: "onboardItem",
+                        route: "onboard",
+                        icon: "N",
+                        title: "Onboard New PPA",
+                        subText: "documents",
+                        badge: "0",
+                        active: false,
+                        isMainMenu:false
+                    },
+                ]
+            };
 
-    aItems.forEach(function (sId) {
+            const oModel = new sap.ui.model.json.JSONModel(oData);
+            this.getView().setModel(oModel, "menu");
 
-        var oItem = this.byId(sId);
+            // setTimeout(() => {
+            //     this._attachMenuClick();
 
-        oItem.addEventDelegate({
-            onclick: function () {
 
-                // ✅ remove active from all
-                this._removeActive();
+            // }, 1000);
+            // this._resetMenu();
 
-                // ✅ set active
-                oItem.addStyleClass("active");
+            //   var oRouter = this.getOwnerComponent().getRouter();
 
-                // ✅ get route from DOM attribute
-                var sRoute = oItem.data("route");
+            //     oRouter.getRoute("Routemain").attachPatternMatched(function () {
+            //         oRouter.navTo("studio", {}, true); // ✅ redirect to studio
+            //     });
 
-                // ✅ navigation
-                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-                oRouter.navTo(sRoute);
+        },
 
-            }.bind(this)
+        _resetMenu: function () {
+            var oMenu = this.byId("studio");
+            if (oMenu) {
+                oMenu.setSelectedKey("");
+            }
+        },
+
+onAfterRendering: function () {
+
+    const oContainer = this.byId("menuContainer12");
+    const aItems = oContainer.getItems();
+
+    aItems.forEach(function (oItem) {
+
+        // ✅ remove old binding (avoid duplicate)
+        oItem.detachBrowserEvent("click");
+
+        oItem.attachBrowserEvent("click", (oEvent) => {
+
+            const oContext = oItem.getBindingContext("menu");
+            const sRoute = oContext.getProperty("route");
+
+            const oModel = this.getView().getModel("menu");
+            const data = oModel.getData();
+
+            // ✅ update active state
+            data.menuItems.forEach(item => item.active = false);
+            oContext.getObject().active = true;
+
+            oModel.refresh();
+
+            // ✅ navigation
+            sap.ui.core.UIComponent
+                .getRouterFor(this)
+                .navTo(sRoute);
+
         });
 
     }.bind(this));
+    this.onOpenDialog();
 },
 
-_removeActive: function () {
+onAfterOpenDialog: function () {
 
-    var aItems = ["studioItem", "inputItem", "outputItem","OnboardItem", "appsItem"];
+    const aItems = this.byId("menuContainer122").getItems();
 
-    aItems.forEach(function (sId) {
-        this.byId(sId).removeStyleClass("active");
-    }.bind(this));
+    aItems.forEach((oItem) => {
+
+        oItem.detachBrowserEvent("click");
+
+        oItem.attachBrowserEvent("click", async () => {
+
+            const oContext = oItem.getBindingContext("menu");
+            const sRoute = oContext.getProperty("route");
+
+            const oModel = this.getView().getModel("menu");
+            const data = oModel.getData();
+
+            // ✅ active state
+            data.menuItems.forEach(item => item.active = false);
+            oContext.getObject().active = true;
+            oModel.refresh();
+
+            // ✅ ONLY update content area
+            const oContainer = this.byId("viewContainer");
+            oContainer.removeAllItems();
+
+            // ✅ LOAD VIEW (THIS IS CORRECT FOR DIALOG)
+            const oView = await sap.ui.core.mvc.XMLView.create({
+                viewName: this._getViewNameFromRoute(sRoute)
+            });
+
+            this.getView().addDependent(oView);
+
+            oContainer.addItem(oView);
+
+        });
+
+    });
+}
+
+,
+
+_getViewNameFromRoute: function (sRoute) {
+
+    const mRoutes = {
+        "onboard": "gmrproject.view.Onboard",
+        "input": "gmrproject.view.Input",
+        "output": "gmrproject.view.Output",
+        "studio": "gmrproject.view.Studio",
+        "pdf": "gmrproject.view.PDF"
+    };
+
+    return mRoutes[sRoute] || "gmrproject.view.Default";
 },
 
-        onMenuClick: function (oEvent) {
-    var sKey = oEvent.getSource().data("menuKey");
 
-    console.log("Clicked:", sKey);
+onOpenDialog: function () {
+  
+const oPlus = this.byId("plusIconBtn");
 
-    // optional navigation
-    var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+    if (oPlus) {
+        oPlus.detachBrowserEvent("click");
 
-    if (sKey === "studio") {
-        oRouter.navTo("main");
-    } else if (sKey === "input") {
-        oRouter.navTo("input");
-    } else if (sKey === "output") {
-        oRouter.navTo("output");
-    } 
-     else if (sKey === "onboard") {
-        oRouter.navTo("onboard");
-    } 
-    else if (sKey === "apps") {
-        oRouter.navTo("apps");
+        oPlus.attachBrowserEvent("click", () => {
+
+            // ✅ Open dialog
+            this.byId("myDialog").open();
+
+    this.onAfterOpenDialog(); // ✅ important
+
+            // OR navigation
+            // sap.ui.core.UIComponent.getRouterFor(this).navTo("more");
+
+        });
     }
+
+},
+onCloseDialog: function () {
+    this.byId("myDialog").close();
 },
 
+       
 
 
     });
